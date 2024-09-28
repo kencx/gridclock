e06c08a5beb3a310ee914590f4b8d2e3ed5e1f8b
kencx <kennethcheo@pm.me>
2024-09-29 02:39:09 +0800

    Implement true gridclock

---
+++ README.md
@@ -0,0 +1,10 @@
+# gridclock
+
+<p align="center">
+	<img src="https://github.com/kencx/gridclock/blob/master/ss.png?raw=true">
+</p>
+
+## Credits
+
+- [Time as a Grid](https://joshleeb.com/posts/gridclock.html)
+- [XXIIVV - time](https://wiki.xxiivv.com/site/time.html)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"log"
+	"strconv"
 	"time"
 
 	ui "github.com/gizak/termui/v3"
@@ -11,151 +12,114 @@ import (
 
 const (
 	// https://symbl.cc/en/unicode/blocks/box-drawing/
-	HLINE   = '━'
-	VLINE   = '┃'
-	rcorner = '┏'
-	lcorner = '┫'
-	tcorner = '┳'
-	bcorner = '┻'
-	xcorner = '╋'
+	HLINE        = '━'
+	VLINE        = '┃'
+	VLINE_DASHED = '┇'
+	RCORNER      = '┣'
+	TCORNER      = '┳'
+	XCORNER      = '╋'
 
 	CLOCK_FORMAT = "15:04:05"
 )
 
 type Clock struct {
 	ui.Block
-	internalClock *Clock
-
-	Height, Width int
-
-	// no of partitions
-	xpart, ypart int
-
-	// starting offset
-	xstart, ystart int
-	// running offset
-	xoffset, yoffset int
-
-	// size of each step
-	xstep, ystep int
-
-	// time interval between steps
-	interval time.Duration
-	counter  int
 }
 
-func NewClock(height, width, xpart, ypart int, interval time.Duration) *Clock {
-	x := width / xpart
-	y := height / ypart
-
-	c := &Clock{
-		Block: *ui.NewBlock(),
-
-		Height: height,
-		Width:  width,
-
-		xpart: xpart,
-		ypart: ypart,
-
-		// spaces out the partitions more evenly
-		xstart: x/2 + 1,
-		ystart: y/2 + 1,
-		// xstart: int(math.Round(float64(x)/2) + 0.5),
-		// ystart: int(math.Round(float64(y)/2) + 0.5),
-
-		xstep: x,
-		ystep: y,
-
-		interval: interval,
-		counter:  0,
-	}
-
-	c.xoffset = c.xstart
-	c.yoffset = c.ystart
-	c.Title = fmt.Sprintf("%s width: %d, height: %d; xoffset: %d; yoffset: %d; elapsed: %d",
-		time.Now().Format(CLOCK_FORMAT),
-		c.Width,
-		c.Height,
-		c.xoffset,
-		c.yoffset,
-		c.counter,
-	)
-
+func NewClock(height, width int) *Clock {
+	c := &Clock{Block: *ui.NewBlock()}
 	c.SetRect(0, 0, width, height)
 	return c
 }
 
 func (c *Clock) Resize(width, height int) {
-	x := width / c.xpart
-	y := height / c.ypart
-
-	c.Height = height
-	c.Width = width
-
-	c.xstart = x/2 + 1
-	c.ystart = y/2 + 1
-
-	c.xstep = x
-	c.ystep = y
-
 	c.SetRect(0, 0, width, height)
 	ui.Clear()
 	ui.Render(c)
 }
 
 func (c *Clock) Tick() {
-	if c.ystep < c.Height {
-		c.yoffset += c.ystep
-	}
-
-	if c.yoffset >= c.Height-1 {
-		c.yoffset = c.ystart
-
-		if c.xstep < c.Width {
-			c.xoffset += c.xstep
-		}
-
-		if c.xoffset >= c.Width-1 {
-			c.xoffset = c.xstart
-		}
-	}
-
-	c.counter += 1
-	c.Title = fmt.Sprintf("%s width: %d, height: %d; xoffset: %d; yoffset: %d; elapsed: %d",
-		time.Now().Format(CLOCK_FORMAT),
-		c.Width,
-		c.Height,
-		c.xoffset,
-		c.yoffset,
-		c.counter,
-	)
 	ui.Render(c)
 }
 
+func drawVLine(buf *ui.Buffer, rune rune, y1, y2, x int) {
+	for i := y1; i < y2-1; i++ {
+		// skip drawing on border
+		if x == 0 {
+			continue
+		}
+		buf.SetCell(ui.NewCell(rune), image.Point{x, i})
+	}
+}
+
+func drawHLine(buf *ui.Buffer, rune rune, x1, x2, y int) {
+	for j := x1; j < x2-1; j++ {
+		// skip drawing on border
+		if y == 0 {
+			continue
+		}
+		buf.SetCell(ui.NewCell(rune), image.Point{j, y})
+	}
+}
+
 func (c *Clock) Draw(buf *ui.Buffer) {
+	var left, top int
+	now := time.Now()
+
 	c.Block.Draw(buf)
 
-	// draw vertical
-	for i := 1; i < c.Height-1; i++ {
-		// skip drawing on border
-		if c.xoffset == 0 {
-			continue
-		}
-		buf.SetCell(ui.NewCell(VLINE), image.Point{c.xoffset, i})
+	title := fmt.Sprintf("%s", now.Format(CLOCK_FORMAT))
+	buf.SetString(title, ui.NewStyle(ui.ColorWhite), image.Point{c.Min.X + 2, c.Max.Y - 1})
+
+	// hour line
+	hour := now.Hour()
+	hourY := c.Max.Y * ((hour % 12) + 1) / (12 + 1)
+	drawHLine(buf, HLINE, left+3, c.Max.X, hourY)
+
+	var hourStr string
+	if hour > 12 {
+		hourStr = strconv.Itoa(hour)
+	} else if (hour % 12) < 10 {
+		hourStr = fmt.Sprintf("0%d", hour)
+	} else {
+		hourStr = strconv.Itoa(hour)
 	}
 
-	// draw horizontal
-	for j := 1; j < c.Width-1; j++ {
-		// skip drawing on border
-		if c.yoffset == 0 {
-			continue
-		}
-		buf.SetCell(ui.NewCell(HLINE), image.Point{j, c.yoffset})
-	}
+	buf.SetString(hourStr, ui.NewStyle(ui.ColorWhite), image.Point{left, hourY})
+	top += hourY
 
-	if c.xoffset != 0 && c.yoffset != 0 {
-		buf.SetCell(ui.NewCell(xcorner), image.Point{c.xoffset, c.yoffset})
+	// minute lines
+	minute := now.Minute()
+	minX := (c.Max.X - left) * ((minute / 5) + 1) / (12 + 1)
+	minY := (c.Max.Y - top) * ((minute % 5) + 1) / (5 + 1)
+
+	drawHLine(buf, HLINE, left+minX, c.Max.X, top+minY)
+	drawVLine(buf, VLINE, top, c.Max.Y, left+minX)
+	drawVLine(buf, VLINE_DASHED, 1, top+1, left+minX)
+
+	buf.SetCell(ui.NewCell(RCORNER), image.Point{left + minX, top + minY})
+	buf.SetCell(ui.NewCell(XCORNER), image.Point{left + minX, top})
+
+	var minStr string
+	if minute < 10 {
+		minStr = fmt.Sprintf("0%d", minute)
+	} else {
+		minStr = strconv.Itoa(minute)
 	}
+	buf.SetString(minStr, ui.NewStyle(ui.ColorWhite), image.Point{left + minX, 0})
+
+	left += minX
+	top += minY
+
+	// second lines
+	secX := (c.Max.X - left) * ((now.Second() / 5) + 1) / (12 + 1)
+	secY := (c.Max.Y - top) * ((now.Second() % 5) + 1) / (5 + 1)
+
+	drawHLine(buf, HLINE, left+secX, c.Max.X, top+secY)
+	drawVLine(buf, VLINE, top, c.Max.Y, left+secX)
+
+	buf.SetCell(ui.NewCell(RCORNER), image.Point{left + secX, top + secY})
+	buf.SetCell(ui.NewCell(TCORNER), image.Point{left + secX, top})
 }
 
 func main() {
@@ -165,7 +129,7 @@ func main() {
 	defer ui.Close()
 
 	width, height := ui.TerminalDimensions()
-	clock := NewClock(height, width, 60, 12, time.Hour)
+	clock := NewClock(height, width)
 	ui.Render(clock)
 
 	uiEvents := ui.PollEvents()
